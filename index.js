0e6b782d66ee8b88789737161b70a37373cf16b5
David Baden <davebaden@gmail.com>
2023-11-01 15:08:43 -0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function (e) {
+    const header = document.getElementById("text");
+    header.innerHTML= "This is really cool!";
+    });
